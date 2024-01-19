b35ea54af3dbc60a81aced11ba102c6037f031f2
Rodrigo Rivas <rodroy019@hotmail.com>
2024-01-19 02:36:26 -0300

    Create: se crea repositorio para el desafio 0302

---
+++ assets/script/script.js
@@ -0,0 +1,29 @@
+//Declaracion de variales y rutas
+imgClick = document.getElementById('imgClick')
+
+inputStickers01 = document.getElementById('inputStickers01')
+inputStickers02 = document.getElementById('inputStickers02')
+inputStickers03 = document.getElementById('inputStickers03')
+spanStickers = document.getElementById('spanStickers')
+buttonStickers = document.getElementById('buttonStickers')
+
+codigo1 = document.getElementById('codigo1')
+codigo2 = document.getElementById('codigo2')
+codigo3 = document.getElementById('codigo3')
+spanPassword = document.getElementById('spanPassword')
+buttonPassword = document.getElementById('buttonPassword')
+
+//Carga de option
+
+//funciones Click
+imgClick.addEventListener("click", function(e){
+    alert("funciona");
+});
+
+buttonStickers.addEventListener("click", function(e){
+    alert("funciona");
+});
+
+buttonPassword.addEventListener("click", function(e){
+    alert("funciona");
+});
